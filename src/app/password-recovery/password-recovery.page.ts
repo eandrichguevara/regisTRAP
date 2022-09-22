e06400be774e092e6b1880ea288a2bc9121a56e7
Vina del Mar <vina@duoc.acad>
2022-09-22 20:31:27 -0300

    Login y recover pages ready

---
+++ src/app/password-recovery/password-recovery.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-password-recovery',
+  templateUrl: './password-recovery.page.html',
+  styleUrls: ['./password-recovery.page.scss'],
+})
+export class PasswordRecoveryPage implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+  
+
+  recoverPassword(){
+    this.router.navigate(['/login'])
+  }
+}
